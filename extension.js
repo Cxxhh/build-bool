f0d40f45c0ce39535cf01355379db391a9084ac5
James Aprosail <aprosail@outlook.com>
2024-05-09 10:53:48 +0800

    hello world in js

---
+++ extension.js
@@ -0,0 +1,11 @@
+const vscode = require("vscode");
+
+function activate(context) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("validate", () => {
+      vscode.window.showInformationMessage("it works");
+    })
+  );
+}
+
+module.export = { activate };
